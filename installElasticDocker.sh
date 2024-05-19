f90e8268922b74c688802f61b6878abc3fb23026
eyeclept <eyeclept@pm.me>
2024-05-19 11:33:34 -0400

    added changed install scripts

--- installElasticDocker.sh
+++ installElasticDocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker pull docker.elastic.co/elasticsearch/elasticsearch:8.4.3
 docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.4.3
-#docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
+docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-reset-password -b -u elastic
+docker cp elasticsearch:/usr/share/elasticsearch/config/certs/http_ca.crt .
