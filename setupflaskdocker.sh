a3f126345784495fd554b12a49fc5fbf23dd6059
eyeclept <eyeclept@pm.me>
2024-05-19 15:14:35 -0400

    fixed and tested docker compose

--- setupflaskdocker.sh
+++ setupflaskdocker.sh
@@ -7,8 +7,11 @@ cd ..
 cd ./reactapp
 docker build -t react:latest .
 cd ..
-#launch docker
-docker-compose up
-#pull stuff for flask
-docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-reset-password -b -u elastic
-docker cp elasticsearch:/usr/share/elasticsearch/config/certs/http_ca.crt .ca.crt
+#launch docker compose
+docker-compose up -d
+#setup elastic
+docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.4.3
+sleep 8
+docker exec -it elasticsearch /usr/share/elasticsearch/bin/elasticsearch-reset-password -b -u elastic | grep "New value:" | awk '{print $3}' > password.txt
+docker cp elasticsearch:/usr/share/elasticsearch/config/certs/http_ca.crt ./ca.crt
+
